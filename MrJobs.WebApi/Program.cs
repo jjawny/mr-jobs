ea1f95201b01802e2cdcc01bc0f86c5b98a52b1b
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-25 23:58:30 +1000

    better route grouping

--- MrJobs.WebApi/Program.cs
+++ MrJobs.WebApi/Program.cs
@@ -1,45 +1,41 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Identity.Web;
+using MrJobs.WebApi.Features;
 using MrJobs.WebApi.Routes;
 
-// TODO: Review this minimal ASP.NET example... needs a cleanup
 
 // Configure the DI container
 var builder = WebApplication.CreateBuilder(args);
 {
   var appSettings = builder.Configuration;
-
   var isDevelopment = builder.Environment.IsDevelopment();
+
   if (isDevelopment)
   {
   }
   else
   {
-    // TODO: Re-do docs here, but basically we don't demo this bcuz AzKeyVault costs $$$
-    // OVERRIDE APPSETTINGS with VALUES from AZURE KEY VAULT
+    // AZURE KEY VAULT EXAMPLE for OVERRIDING APPSETTINGS
     //  - See AzKeyVault > Access policies > Add access policy (allow web app to access the vault using Managed Identity)
-    //  - See AzKeyVault > Objects > Secrets (define secrets)
-    //  - GOTCHA: Colons are not supported in AzKeyVault, so use double dashes (--) for colons
-    //  - EXAMPLE: for a secret named "SystemRoutes--ApiKey" translates to "SystemRoutes:ApiKey" which will override
-    //    {
-    //      "SystemRoutes": {
-    //        "ApiKey": "<OVERRIDEN>"
-    //      }
-    //    }
+    //  - See AzKeyVault > Objects > Secrets (populate secrets)
+    //  - Gotcha: Colons are not supported in AzKeyVault, so use double dashes (--) for colons
+    //            For example, secret "System--ApiKey" will override appsettings.json:
+    //            {
+    //              "System": {
+    //                "ApiKey": "<OVERRIDEN>"
+    //              }
+    //            }
 
+    // INACTIVE CODE as AzKeyVault has no free tier:
     // var azKeyVaultUri = new Uri(appSettings["AzKeyVault:Uri"]);
-    // var client = new SecretClient(azKeyVaultUri, new DefaultAzureCredential());
-    // appSettings.AddAzureKeyVault(client, new KeyVaultSecretManager());
+    // var secretsClient = new SecretClient(azKeyVaultUri, new DefaultAzureCredential());
+    // appSettings.AddAzureKeyVault(secretsClient, new KeyVaultSecretManager());
   }
 
   builder.Services.AddOpenApi();
 
-  builder.Services
-  //.AddAuthentication().AddJwtBearer(options => appSettings.GetSection("Auth").Bind(options)); // JWT validation for generic providers (Firebase etc)
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(appSettings.GetSection("AzAuth")); // JWT validation for Azure Entra ID
-
-  // RequireAuthorization on all routes by default (opt-out using AllowAnonymous)
+  builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(appSettings.GetSection("AzAuth"));
   builder.Services.AddAuthorizationBuilder()
     .SetFallbackPolicy(new AuthorizationPolicyBuilder()
       .RequireAuthenticatedUser()
@@ -50,6 +46,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 {
   var isDevelopment = app.Environment.IsDevelopment();
+
   if (isDevelopment)
   {
     app.MapOpenApi();
@@ -60,17 +57,26 @@ var app = builder.Build();
   }
 
   app.UseHttpsRedirection();
+  app.UseExceptionHandler("/error");
 
+  // Auth all routes by default (opt-out for anons)
   app.UseAuthentication();
   app.UseAuthorization();
 
-  static string HealthCheck() => "Healthy!";
-  app.MapGet("/", HealthCheck).AllowAnonymous();
-  app.MapGet("/health", HealthCheck).AllowAnonymous();
-  app.MapGet("/healthcheck", HealthCheck).AllowAnonymous();
+  var healthCheck = app.MapGroup("/");
+  healthCheck.MapGet("", HealthChecks.Handle).AllowAnonymous();
+  healthCheck.MapGet("health", HealthChecks.Handle).AllowAnonymous();
 
-  app.MapGet("/poke", PokeRoute.Handle);
-  app.MapGet("/access-via-custom-api-key", SystemJobRoute.Handle).AllowAnonymous();
+  var globalError = app.MapGroup("/error");
+  globalError.MapGet("", GlobalError.Handle).AllowAnonymous();
+  globalError.MapGet("test", GlobalErrorTest.Handle).AllowAnonymous();
+
+  var pokeFeatures = app.MapGroup("/poke");
+  pokeFeatures.MapGet("", Poke.Handle);
+  pokeFeatures.MapGet("using-api-key", PokeAsSystem.Handle).AllowAnonymous();
 
   app.Run();
 }
+
+// Make Program class accessible for integration tests
+public partial class Program { }
