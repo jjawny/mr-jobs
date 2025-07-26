9c5c6bcf46eb89e8a296e720ecf9c7530bfe24ed
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-26 13:35:00 +1000

    validated powershell version working

---
+++ MrJobs.WebJob.PowerShell/README.md
@@ -0,0 +1,5 @@
+# How to run?
+Example not deployed to AzWebJob (like the console app version), instead test locally:
+1. `dotnet run` [MrJobs.WebApi](../MrJobs.WebApi/)
+2. Replace "{{SYSTEM_API_KEY}}" in [call_mrjobs_webapi.ps1](./call_mrjobs_webapi.ps1)
+3. `pwsh call_mrjobs_webapi.ps1`
--- MrJobs.WebJob.PowerShell/call_mrjobs_webapi.ps1
+++ MrJobs.WebJob.PowerShell/call_mrjobs_webapi.ps1
@@ -1,19 +1,23 @@
 $systemApiKey = "{{SYSTEM_API_KEY}}"
-$systemApiKeyHeader = "x-system-api-key"
+$headers = @{}
+$headers["x-system-api-key"] = $systemApiKey
+$headers["Accept"] = "application/json"
 $timeoutSec = 30
 $maxRetries = 3
 $retryCount = 0
 
 while ($retryCount -lt $maxRetries) {
   try {
-    $response = Invoke-RestMethod -Uri "https://{{API_HOST}}/poke/using-api-key"
-                                  -Method Get
-                                  -Headers @{ $systemApiKeyHeader = $systemApiKey }
+    $response = Invoke-RestMethod -Uri "http://localhost:5138/poke/using-api-key" `
+                                  -Method Get `
+                                  -Headers $headers `
                                   -TimeoutSec $timeoutSec
 
-    Write-Output: "Success, API response: $response"
+    Write-Host "Success:"
+    $response | ConvertTo-Json -Depth 5
+    break
   } catch {
-    Write-Host "Error occurred: $_"
+    Write-Host "Error: $_"
     Start-Sleep -Seconds 5
     $retryCount++
   }
